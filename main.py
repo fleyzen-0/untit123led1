cceff61633e835b674cf6e3762f91c0531dbdc98
andrii <andrijkorolevic@gmail.com>
2022-10-15 09:36:05 +0300

    crete lab_ex_1

--- main.py
+++
@@ -1,8 +0,0 @@
-print("Висота квадрата: ")
-number1 = int(input())
-print("Ширина квадрата: ")
-number2 = int(input())
-for i in range(number1):
-        for j in range(number2):
-            print ("*", end = " " )
-        print()
