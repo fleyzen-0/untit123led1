4ee49e88d6bcc611b746c885124842d492ff06dd
andrii <andrijkorolevic@gmail.com>
2022-10-15 09:35:35 +0300

    done lab_ex_2

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Висота квадрата: ")
+number1 = int(input())
+print("Ширина квадрата: ")
+number2 = int(input())
+for i in range(number1):
+        for j in range(number2):
+            print ("*", end = " " )
+        print()
